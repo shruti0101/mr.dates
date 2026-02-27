713bb375bfe2884952d474968ee852c94fa98440
shruti0101 <shrutiguptacu@gmail.com>
2026-02-27 17:52:27 +0530

    error fix

---
+++ middleware.js
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+
+export function middleware(request) {
+  const host = request.headers.get("host");
+
+  if (
+    host !== "mrdates.in" &&
+    host !== "www.mrdates.in"
+  ) {
+    return new NextResponse("Forbidden", { status: 403 });
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: "/:path*",
+};

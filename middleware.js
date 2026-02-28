efbc86348c7549f2d5314fd74d9bfde068d675b0
shruti0101 <shrutiguptacu@gmail.com>
2026-02-28 10:25:54 +0530

    changes

--- middleware.js
+++ middleware.js
@@ -1,18 +1,18 @@
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
 
-export function middleware(request) {
-  const host = request.headers.get("host");
+// export function middleware(request) {
+//   const host = request.headers.get("host");
 
-  if (
-    host !== "mrdates.in" &&
-    host !== "www.mrdates.in"
-  ) {
-    return new NextResponse("Forbidden", { status: 403 });
-  }
+//   if (
+//     host !== "mrdates.in" &&
+//     host !== "www.mrdates.in"
+//   ) {
+//     return new NextResponse("Forbidden", { status: 403 });
+//   }
 
-  return NextResponse.next();
-}
+//   return NextResponse.next();
+// }
 
-export const config = {
-  matcher: "/:path*",
-};
+// export const config = {
+//   matcher: "/:path*",
+// };

d9fa773afad36b3e75d1ae5c1f643590019f7e79
manish kumar <manishkumarstudent8744@gmail.com>
2026-05-27 15:40:53 +0530

    image chnages and header

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -89,30 +89,30 @@ useEffect(() => {
     {/* SHOP TAB */}
     <Link
       href="/products"
-      className="relative w-1/2 h-full flex items-center justify-center bg-[#f3ecef] text-black font-extrabold uppercase tracking-[0.2em] text-lg z-10"
+      className="relative w-1/2 h-full flex items-center justify-center bg-[#f3ecef] group hover:bg-black transition-colors duration-150 text-black font-extrabold uppercase tracking-[0.2em] text-lg z-10"
     >
       {/* angled shape */}
       <div
-        className="absolute right-[-40px] top-0 h-full w-[80px] bg-[#f3ecef]"
+        className="absolute right-[-40px] transition-colors duration-150 group group-hover:bg-black top-0 h-full w-[80px] bg-[#f3ecef]"
         style={{
           clipPath: "polygon(0 0, 100% 0, 0 100%)",
         }}
       />
 
       <span className="relative flex items-center gap-2">
-        <ShoppingBag size={20} strokeWidth={2.5} />
-        <span>Shop</span>
+        <ShoppingBag size={20} className="group-hover:text-[#f3ecef]" strokeWidth={2.5} />
+        <span className=" group-hover:text-[#f3ecef] ">Shop</span>
       </span>
     </Link>
 
     {/* LEARN TAB */}
     <Link
       href="/learn"
-      className="relative w-1/2 h-full flex items-center justify-center bg-[#8d2957] text-white font-extrabold uppercase tracking-[0.2em] text-lg"
+      className="relative w-1/2 h-full flex items-center justify-center group bg-[#8d2957] transition-colors duration-150 hover:bg-[#f3ecef] text-white font-extrabold uppercase tracking-[0.2em] text-lg"
     >
       <span className="flex items-center gap-2">
-        <BookOpen size={20} strokeWidth={2.5} />
-        <span className="underline underline-offset-4">
+        <BookOpen size={20} className="group-hover:text-[#8d2957] transition-colors duration-150" strokeWidth={2.5} />
+        <span className="underline group-hover:text-[#8d2957] transition-colors duration-150 underline-offset-4">
           Learn
         </span>
       </span>
